3eea2ea8ceae4a9c888754f93f8f102c95d2e450
Jasper Van Dyck <vandyckja@delawareconsulting.com>
2024-03-11 13:29:22 +0100

    some vote card styling

--- src/components/Vote.tsx
+++ src/components/Vote.tsx
@@ -2,7 +2,6 @@ import { causes } from "@/constants";
 import { Button } from "./ui/button";
 import {
   Card,
-  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
@@ -15,54 +14,62 @@ import {
   DrawerDescription,
   DrawerFooter,
   DrawerHeader,
-  DrawerTitle,
   DrawerTrigger,
 } from "./ui/drawer";
+import { useState } from "react";
+
+type Cause = {
+  title: string;
+  description: string;
+  image: string;
+};
 
 function Vote() {
+
+  const [cause, setCause] = useState<Cause | null>(null);
+
   return (
     <>
       <Drawer>
-        <div>
-          <h1 className="text-4xl font-bold text-center">Choose your cause</h1>
-          <p className="text-center mt-8 text-lg">
-            The National Lottery supports various community causes. You can vote
-            for your favorite cause to receive extra funding.
-          </p>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-16">
-          {causes.map((cause, index) => (
-            <Card className="shadow-md" key={index}>
-              <CardHeader>
-                <CardTitle>{cause.title}</CardTitle>
-                <CardDescription>{cause.description}</CardDescription>
-              </CardHeader>
-              <CardContent></CardContent>
-              <CardFooter className="flex justify-end align-bottom">
-                <DrawerTrigger asChild>
-                  <Button variant="outline" className="px-16 py-4">
-                    Vote
-                  </Button>
-                </DrawerTrigger>
-              </CardFooter>
-            </Card>
-          ))}
-        </div>
         <DrawerContent>
-          <div className="mx-auto w-full max-w-sm">
-            <DrawerHeader>
-              <DrawerTitle>Congratulations</DrawerTitle>
-              <DrawerDescription>
-                You've succesfully voted for you community cause.
-              </DrawerDescription>
-            </DrawerHeader>
-            <DrawerFooter>
-              <DrawerClose asChild>
-                <Button variant="outline">I understand</Button>
-              </DrawerClose>
-            </DrawerFooter>
+            <div className="mx-auto w-full max-w-sm py-4">
+              <DrawerHeader className="gap-4">
+                <img src={cause?.image}></img>
+                <DrawerDescription>
+                  {cause?.description}
+                </DrawerDescription>
+              </DrawerHeader>
+              <DrawerFooter>
+                <DrawerClose>
+                  <Button className="w-full">I choose this community cause</Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </div>
+          </DrawerContent>
+          <div>
+            <h1 className="text-4xl font-bold text-center">Choose your cause</h1>
+            <p className="text-center mt-8 text-lg">
+              The National Lottery supports various community causes. You can vote
+              for your favorite cause to receive extra funding.
+            </p>
           </div>
-        </DrawerContent>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-16">
+            {causes.map((cause, index) => (
+              <Card className="shadow-md flex flex-col justify-between" key={index}>
+                <CardHeader>
+                  <CardTitle>{cause.title}</CardTitle>
+                  <CardDescription>{cause.description}</CardDescription>
+                </CardHeader>
+                <CardFooter className="flex justify-end">
+                  <DrawerTrigger asChild>
+                    <Button className="px-16 py-4 sm:w-fit w-full" onClick={() => setCause(cause)}>
+                      Vote
+                    </Button>
+                  </DrawerTrigger>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
       </Drawer>
     </>
   );
