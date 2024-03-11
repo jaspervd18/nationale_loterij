4f0463b49bfb9133c5480faee10728a9702ef08d
Jasper Van Dyck <vandyckja@delawareconsulting.com>
2024-03-11 13:28:31 +0100

    padding on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
     <footer className="border-t border-primary bg-primary">
-      <div className="mx-auto max-w-7xl px-6 py-6 md:flex md:items-center md:justify-between lg:px-8">
-        <div className="mt-8 md:order-1 md:mt-0">
+      <div className="mx-auto max-w-7xl px-6 py-4 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="mt-4 md:order-1 md:mt-0">
           <p className="text-center text-sm leading-5 text-white">
             &copy; 2024 Nationale Loterij
           </p>
