ff36e12b553ca1edc70db65205ab7a22af89e100
Jasper Van Dyck <vandyckja@delawareconsulting.com>
2024-03-11 13:29:48 +0100

    add images to causes

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,4 +2,12 @@ import logo from "./logo.svg";
 import facebook from "./facebook.png";
 import instagram from "./instagram.png";
 
-export { logo, facebook, instagram };
+import wwf from "./wwf.png";
+import cancerResearch from "./cancer-research.png";
+import makeAWish from "./make-a-wish.png";
+import oxfam from "./oxfam.png";
+import redCross from "./red-cross.png";
+import saveTheChildren from "./save-the-children.png";
+import unicef from "./unicef.png";
+
+export { logo, facebook, instagram, wwf, cancerResearch, makeAWish, oxfam, redCross, saveTheChildren, unicef};
