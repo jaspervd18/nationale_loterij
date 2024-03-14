417b88c294190e17e021ba955ddc6e902ab3a220
Jasper Van Dyck <vandyckja@delawareconsulting.com>
2024-03-14 14:22:58 +0100

    faq

---
+++ src/components/FAQ.tsx
@@ -0,0 +1,24 @@
+import { faq } from "@/constants";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./ui/accordion";
+
+function FAQ() {
+  return (
+    <>
+      <h2 className="text-2xl font-bold mt-8">Frequently asked questions</h2>
+
+      <Accordion type="single" collapsible>
+          {faq.map((item, index) => {
+            return (
+              <AccordionItem key={index} value={`item-${index}`}>
+                <AccordionTrigger>{item.question}</AccordionTrigger>
+                <AccordionContent>{item.answer}</AccordionContent>
+              </AccordionItem>
+            );
+          })}
+      </Accordion>
+    </>
+  );
+}
+
+export default FAQ;
+
