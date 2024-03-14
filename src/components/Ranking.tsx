335f65a81b0cff99a7ebdf180f8e3a2012c75192
Jasper Van Dyck <vandyckja@delawareconsulting.com>
2024-03-14 15:27:48 +0100

    Created rankgin

--- src/components/Ranking.tsx
+++ src/components/Ranking.tsx
@@ -1,7 +1,38 @@
+import { ranking } from "@/constants";
+import { cn } from "@/lib/utils";
+
 function Ranking() {
     return (
         <>
-            <div>Hier komt gekke ranking</div>
+            <div>
+                <h1 className="text-4xl font-bold text-center">Monthly ranking</h1>
+                <p className="text-center mt-8 text-lg">
+                    Increase your chances at winning some of our amazing prizes by participating in our community causes 
+                    and earning points. The more points you earn, the higher you'll rank on our monthly leaderboard. 
+                    Good luck!
+                </p>
+            </div>
+            <div className="shadow-md rounded-sm w-1/2 my-16">
+                <div className="flex justify-around font-semibold py-3 rounded-t-sm border-b border-primary bg-primary text-white">
+                    <p>Rank</p>
+                    <p>Username</p>
+                    <p>Score</p>
+                </div>
+                <div className="flex flex-col">
+                    {ranking.sort((u1, u2) => u2.amount - u1.amount).map((user, index) => (
+                        <div key={index} className={cn(index % 2 == 0 && "bg-gray-100","flex justify-between py-2 px-24")}>
+                            <p className="text-center font-bold">{index + 1}</p>
+                            <p className="text-center">{user.name}</p>
+                            <p className="text-center">{user.amount}</p>
+                        </div>
+                    ))}
+                    <div className="bg-gray-100 flex justify-between py-1 px-24">
+                            <p className="text-center">...</p>
+                            <p className="text-center">...</p>
+                            <p className="text-center">...</p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
