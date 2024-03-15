26c4cc7b8501ad15f77ef3dd5de16173340fea71
Jasper Van Dyck <vandyckja@delawareconsulting.com>
2024-03-15 08:31:08 +0100

    Created sample badge

--- src/components/Ranking.tsx
+++ src/components/Ranking.tsx
@@ -1,6 +1,7 @@
-import { ranking } from "@/constants";
-import { cn } from "@/lib/utils";
+import { BadgeCent } from "lucide-react";
 import Countdown from "./Countdown";
+import PlayerRanking from "./PlayerRanking";
+import { Card, CardContent, CardTitle } from "./ui/card";
 
 function Ranking() {
 
@@ -18,31 +19,28 @@ function Ranking() {
             </div>
             <div className="flex gap-2 my-16">
                 <div className="shadow-lg rounded-sm w-1/2">
-                <div className="flex justify-around font-semibold py-3 rounded-t-sm border-b border-primary bg-primary text-white">
-                    <p>Rank</p>
-                    <p>Username</p>
-                    <p>Score</p>
+                    <PlayerRanking />
                 </div>
-                <div className="flex flex-col">
-                    {ranking.sort((u1, u2) => u2.amount - u1.amount).map((user, index) => (
-                        <div key={index} className={cn(index % 2 == 0 && "bg-gray-100","flex justify-between py-2 px-24")}>
-                            <p className="text-center font-bold">{index + 1}</p>
-                            <p className="text-center">{user.name}</p>
-                            <p className="text-center">{user.amount}</p>
-                        </div>
-                    ))}
-                    <div className="bg-gray-100 flex justify-between py-1 px-24">
-                        <p className="text-center">...</p>
-                        <p className="text-center">...</p>
-                        <p className="text-center">...</p>
+                <div className="w-1/2 flex flex-col gap-2">
+                    <div>
+                        <Countdown targetDate={targetDate} />
+                    </div>
+                    <div className="">
+                        <Card>
+                            <CardTitle className="text-center my-6">Achievements</CardTitle>
+                            <CardContent>
+                                <div className="flex flex-row gap-2 justify-around">
+                                    <div className="relative w-16 h-16 bg-primary rounded-3xl transform rotate-45">
+                                        <div className="absolute inset-0 flex justify-center items-center">
+                                            <BadgeCent className="text-white transform -rotate-45" size={48} />
+                                        </div>
+                                    </div>
+                                </div>
+                            </CardContent>
+                        </Card>
                     </div>
                 </div>
-                </div>
-                <div className="w-1/2">
-                    <Countdown targetDate={targetDate} />
-                </div>
             </div>
-            
         </>
     )
 }
