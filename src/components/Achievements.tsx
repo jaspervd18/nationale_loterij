2769aa8253226fbd420d6b3f4a152e738f4c1c68
Jasper Van Dyck <jasper.vandyck@hotmail.be>
2024-03-15 21:47:32 +0100

    Added badges

---
+++ src/components/Achievements.tsx
@@ -0,0 +1,62 @@
+import {
+  BadgeAlert,
+  BadgeCent,
+  BadgeCheck,
+  BadgeIndianRupee,
+  BadgePlus,
+} from "lucide-react";
+import { Card, CardContent, CardTitle } from "./ui/card";
+
+function Achievements() {
+  return (
+    <Card className="shadow-md">
+      <CardTitle className="text-center my-6">Achievements</CardTitle>
+      <CardContent>
+        <div className="flex flex-row gap-2 justify-around">
+          <div className="relative w-16 h-16 bg-primary rounded-3xl transform rotate-45 hover:scale-105 transition duration-200 ease-in-out">
+            <div className="absolute inset-0 flex justify-center items-center">
+              <BadgeCent
+                className="text-white transform -rotate-45"
+                size={48}
+              />
+            </div>
+          </div>
+          <div className="relative w-16 h-16 bg-primary rounded-3xl transform rotate-45 hover:scale-105 transition duration-200 ease-in-out">
+            <div className="absolute inset-0 flex justify-center items-center">
+              <BadgeCheck
+                className="text-white transform -rotate-45"
+                size={48}
+              />
+            </div>
+          </div>
+          <div className="relative w-16 h-16 bg-primary rounded-3xl transform rotate-45 hover:scale-105 transition duration-200 ease-in-out">
+            <div className="absolute inset-0 flex justify-center items-center">
+              <BadgeAlert
+                className="text-white transform -rotate-45"
+                size={48}
+              />
+            </div>
+          </div>
+          <div className="relative w-16 h-16 bg-primary rounded-3xl transform rotate-45 hover:scale-105 transition duration-200 ease-in-out">
+            <div className="absolute inset-0 flex justify-center items-center">
+              <BadgeIndianRupee
+                className="text-white transform -rotate-45"
+                size={48}
+              />
+            </div>
+          </div>
+          <div className="relative w-16 h-16 bg-primary rounded-3xl transform rotate-45 hover:scale-105 transition duration-200 ease-in-out">
+            <div className="absolute inset-0 flex justify-center items-center">
+              <BadgePlus
+                className="text-white transform -rotate-45"
+                size={48}
+              />
+            </div>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Achievements;
