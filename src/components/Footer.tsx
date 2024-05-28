1c903bf92f1268628abc062f8c7518d23b45ccdc
Jasper Van Dyck <jasper.vandyck@hotmail.be>
2024-05-28 10:18:00 +0200

    disclaimer in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,10 @@ function Footer() {
       <div className="mx-auto max-w-7xl px-6 py-4 md:flex md:items-center md:justify-between lg:px-8">
         <div className="mt-4 md:order-1 md:mt-0">
           <p className="text-center text-sm leading-5 text-white">
-            &copy; 2024 Nationale Loterij
+            Disclaimer: I do not own the rights to any of the content used about
+            the Nationale Loterij. This is a fictional website created for the
+            FTI Hackathon in collaboration with the Nationale Loterij. All
+            rights reserved.
           </p>
         </div>
       </div>
